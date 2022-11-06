28c20712a264325929443ba593218de2af03a887
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2022-11-06 20:19:42 +0100

    Monkaa, driving excluded

--- sense/datasets/sceneflow.py
+++ sense/datasets/sceneflow.py
@@ -27,17 +27,17 @@ def make_disp_data(flyingthings3d_dir, monkaa_dir, driving_dir, disp_thresh):
 	print('==> making disparity data on FlyingThings3D.')
 	train_data, test_data = fth3d.make_disp_data(flyingthings3d_dir, disp_thresh)
 
-	print('==> making disparity data on Monkaa.')
-	tmp_train_data, tmp_test_data = mk.make_disp_data(monkaa_dir, disp_thresh)
-	for i in range(2):
-		train_data[i].extend(tmp_train_data[i])
-		test_data[i].extend(tmp_test_data[i])
-
-	print('==> making disparity data on Driving.')
-	tmp_train_data, tmp_test_data = drv.make_disp_data(driving_dir, disp_thresh)
-	for i in range(2):
-		train_data[i].extend(tmp_train_data[i])
-		test_data[i].extend(tmp_test_data[i])
+	# print('==> making disparity data on Monkaa.')
+	# tmp_train_data, tmp_test_data = mk.make_disp_data(monkaa_dir, disp_thresh)
+	# for i in range(2):
+	# 	train_data[i].extend(tmp_train_data[i])
+	# 	test_data[i].extend(tmp_test_data[i])
+# 
+	# print('==> making disparity data on Driving.')
+	# tmp_train_data, tmp_test_data = drv.make_disp_data(driving_dir, disp_thresh)
+	# for i in range(2):
+	# 	train_data[i].extend(tmp_train_data[i])
+	# 	test_data[i].extend(tmp_test_data[i])
 	return train_data, test_data
 
 def make_flow_disp_data(flyingthings3d_dir, monkaa_dir, driving_dir, 
