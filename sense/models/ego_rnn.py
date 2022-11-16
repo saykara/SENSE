bd61c2e95b690d3930fba8f47a43f2535fd1efc0
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2022-11-16 11:07:24 +0100

    Ego_rnn base

---
+++ sense/models/ego_rnn.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from .common_v2 import *
+
+class EgoRnn(nn.Module):
+	def __init__(self, input_dim, hidden_dim=1000, layers=2):
+		super(EgoRnn, self).__init__()
+		self.maxpool = nn.MaxPool2d(2)
+		self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=layers)
+		self.linear1 = nn.Linear(hidden_dim, 128)
+		self.linear2 = nn.Linear(128, 6)
+
+    
+	def forward(self, x):
+        pass
+    
