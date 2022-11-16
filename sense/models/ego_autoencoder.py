d0e1dcd6eea1a959045833ec5b20e141c52f6437
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2022-11-16 11:07:05 +0100

    ego_autoencoder completed

--- sense/models/ego_autoencoder.py
+++ sense/models/ego_autoencoder.py
@@ -31,15 +31,33 @@ class EGOEncoder(nn.Module):
 class EGODecoder(nn.Module):
     def __init__(self, bn_type, act_type):
         super(EGODecoder, self).__init__()
-        self.deconv5 = deconv()
-        convbn( 20,   40, kernel_size=7, stride=2, padding=3, bn_type=bn_type, act_type=act_type)
-        self.flow5   = convbn(256,  512, kernel_size=3, stride=2, padding=1, bn_type=bn_type, act_type=act_type)
-        self.deconv4 = convbn( 64,  128, kernel_size=5, stride=2, padding=2, bn_type=bn_type, act_type=act_type)
-        self.flow4   = convbn(512,  512, kernel_size=3, stride=1, padding=1, bn_type=bn_type, act_type=act_type)
-        self.deconv3 = convbn(128,  256, kernel_size=5, stride=2, padding=2, bn_type=bn_type, act_type=act_type)
-        self.flow3   = convbn(512,  512, kernel_size=3, stride=2, padding=1, bn_type=bn_type, act_type=act_type)
-        self.deconv2 = convbn(256,  256, kernel_size=3, stride=1, padding=1, bn_type=bn_type, act_type=act_type)
-        self.flow2   = convbn(512,  512, kernel_size=3, stride=1, padding=1, bn_type=bn_type, act_type=act_type)
+        self.deconv5 = deconv(1024, 512, kernel_size=4, stride=2, padding=1)
+        self.flow5   = nn.Sequential( 
+                            conv(1024,  2, kernel_size=3, stride=1, padding=1),
+                            nn.Upsample(scale_factor=2, mode='bilinear'))
+        self.deconv4 = deconv(1024, 512, kernel_size=4, stride=2, padding=1)
+        self.flow4   = nn.Sequential( 
+                            conv(1026,  2, kernel_size=3, stride=1, padding=1),
+                            nn.Upsample(scale_factor=2, mode='bilinear'))
+        self.deconv3 = deconv(1026, 256, kernel_size=4, stride=2, padding=1)
+        self.flow3   = nn.Sequential( 
+                            conv(514,  2, kernel_size=3, stride=1, padding=1),
+                            nn.Upsample(scale_factor=2, mode='bilinear'))
+        self.deconv2 = deconv(514, 128, kernel_size=4, stride=2, padding=1)
+        self.flow2   = nn.Sequential( 
+                            conv(258,  2, kernel_size=3, stride=1, padding=1),
+                            nn.Upsample(scale_factor=4, mode='bilinear'))
         
     def forward(self, x):
-        pass
+        c1, c2, c3, c4, c5 = x
+        
+        d5 = self.deconv5(c5)
+        f5 = self.flow5(torch.cat((d5, c4)))
+        d4 = self.deconv4(torch.cat((d5, c4)))
+        f4 = self.flow4(torch.cat((d4, c3, f5)))
+        d3 = self.deconv3(torch.cat((d4, c3, f5)))
+        f3 = self.flow3(torch.cat((d3, c2, f4)))
+        d2 = self.deconv2(torch.cat((d3, c2, f4)))
+        recovered_of = self.flow2(torch.cat((d2, c1, f3)))
+        
+        return recovered_of
