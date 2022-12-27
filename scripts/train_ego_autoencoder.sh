8458bcb9fc49276bcfb744eb8e5d79d3a33b5e3c
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2022-12-27 01:06:02 +0100

    hyper parameters adjusted for colab

--- scripts/train_ego_autoencoder.sh
+++ scripts/train_ego_autoencoder.sh
@@ -9,11 +9,11 @@ python tools/train_ego_autoencoder.py pre-train \
    --flow-crop-imh 384 \
    --flow-crop-imw 640 \
    --savemodel ${RES_DIR} \
-   --workers 0 \
+   --workers 2 \
    --lr 0.0001 \
    --lr-steps 70 \
    --lr-gamma 0.1 \
    --epochs 10 \
    --bn-type syncbn \
-   --batch-size 4 \
+   --batch-size 48 \
    --print-freq 20
