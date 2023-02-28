946976166ad3131f89da4202f3e9c7b2b1903ec2
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2023-02-28 08:19:38 +0100

    save model path ficed

--- scripts/train_synthetic_sceneflow.sh
+++ scripts/train_synthetic_sceneflow.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH=./
 
-RES_DIR=/drive/MyDrive/Thesis/Model
+RES_DIR=/content/drive/MyDrive/Thesis/Model
 BASE_DIR=/content
 
 python tools/train_joint_synthetic_sceneflow.py pre-train \
