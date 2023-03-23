9eec8064f3243d2894e37284c09f57b7220a7e10
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2023-03-23 14:10:11 +0100

    kitti seq 1 added

--- tools/train_ego_autoencoder.py
+++ tools/train_ego_autoencoder.py
@@ -90,7 +90,7 @@ def make_flow_data_helper(args):
     elif args.dataset == "kittimalaga":
         kitti_dir = os.path.join(args.base_dir, "kitti_vo")
         malaga_dir = os.path.join(args.base_dir, "malaga")
-        kitti_train_sequences = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+        kitti_train_sequences = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
         malaga_train_sequences = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
         kitti_train, kitti_test = kitti_vo.kitti_vo_flow_data_helper(kitti_dir, kitti_train_sequences)
         malaga_train, malaga_test = malaga.malaga_flow_data_helper(malaga_dir, malaga_train_sequences)
