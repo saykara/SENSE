6050d3ea24fb5ca6696c8414c19089bf6caafd49
DESKTOP-6D2OS6P\Utkua <utkuanilsaykara@gmail.com>
2023-03-27 15:49:58 +0200

    batch size and epochs updated

--- scripts/train_lstm.sh
+++ scripts/train_lstm.sh
@@ -17,11 +17,11 @@ python tools/train_lstm.py pre-train \
    --savemodel ${RES_DIR} \
    --workers 0 \
    --lr 0.0001 \
-   --lr-steps 70 \
+   --lr-steps 20 \
    --lr-gamma 0.1 \
-   --epochs 72 \
+   --epochs 60 \
    --bn-type syncbn \
-   --batch-size 20 \
+   --batch-size 36 \
    --corr-radius 4  \
    --disp-crop-imh 256 \
    --disp-crop-imw 512 \
